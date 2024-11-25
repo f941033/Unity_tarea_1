733a3224af855a00dce97e8a15de8a1159683895
f941033 <f941033@gmail.com>
2024-11-25 18:30:39 +0100

    Terreno v2

---
+++ Unity_tarea_1/Assets/Scripts/MoveItems.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveItems : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "item")
+        {
+            if(Input.GetMouseButton(0))
+            {
+                //other.transform.parent = transform;
+                other.transform.forward = transform.forward;
+                other.transform.position = transform.position;
+                other.GetComponent<Rigidbody>().useGravity = false;
+            }
+            else
+            {
+                //other.transform.parent = null;                
+                other.GetComponent<Rigidbody>().useGravity = true;
+            }
+        }
+    }
+}
